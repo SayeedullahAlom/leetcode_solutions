7d10414fe95dd13cb6902eb77a05be8dab398500
Sayeedullah <sayeedullahalom@gmail.com>
2026-08-20 19:18:54 +0530

    Time: 4 ms (18.36%), Space: 16 MB (64.6%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -27,7 +27,33 @@ public:
 
         vector<vector<int>> dp(n+1,vector<int>(2,-1));
 
-        return f(0,1,prices,dp);
+        // return f(0,1,prices,dp);
+
+        for(int i=0;i<=1;i++){
+            dp[n][i]=0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0;
+
+                if(buy){
+                    profit=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                else{
+                    if(i==n-1){
+                        profit=max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                    }
+                    else{
+                        profit=max(prices[i]+dp[i+2][1],dp[i+1][0]);
+                    }
+                }
+
+                dp[i][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
         
     }
 };
