af5c69ee079e054dfa6a46aa4b7ab7e9754538e4
Sayeedullah <sayeedullahalom@gmail.com>
2026-08-23 20:13:02 +0530

    Time: 7 ms (39.69%), Space: 10.9 MB (21.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
 
         if(s2.size()<s1.size()) return false;
 
@@ -12,14 +13,20 @@ public:
         int l=0;
         int r=l+s1.size()-1;
 
-        while(r<s2.size()){
-            unordered_map<char,int> m2;
+        for(int i=0;i<s1.size()-1;i++){
+            m2[s2[i]]++;
+        }
 
-            for(int i=l;i<=r;i++){
-                m2[s2[i]]++;
-            }
+        while(r<s2.size()){
+
+            m2[s2[r]]++;
 
             if(m1==m2) return true;
+
+            m2[s2[l]]--;
+            if(m2[s2[l]]==0){
+                m2.erase(s2[l]);
+            }
 
             l++;
             r++;
