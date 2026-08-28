224cce67954fb9af7974a66fc9a04bccc61e7e0c
Sayeedullah <sayeedullahalom@gmail.com>
2026-08-28 22:12:02 +0530

    Time: 0 ms (100%), Space: 21.8 MB (47.88%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,103 @@
+// class Solution {
+// public:
+//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+//         vector<vector<int>> ans;
+//         int i=0;
+//         while(i<intervals.size()){
+//             if(newInterval[0]<intervals[i][0] && newInterval[1]<intervals[i][0]){
+//                 intervals.insert(intervals.begin(),newInterval);
+//                 break;
+//             }
+//             else if(newInterval[0]<intervals[i][0] && newInterval[1]>=intervals[i][0] && newInterval[1]<=intervals[i][1]){
+//                 intervals[i][0]=newInterval[0];
+//                 break;
+//             }
+//             else if(newInterval[0]>=intervals[i][0] && newInterval[0]<=intervals[i][1] && newInterval[1]>intervals[i][1]){
+//                 intervals[i][1]=newInterval[1];
+//                 break;
+//             }
+
+//             i++;
+//         }
+
+//         ans.push_back({intervals[0][0],intervals[0][1]});
+
+//         for(int i=1;i<intervals.size();i++){
+//             int s=ans.size()-1;
+//             if(ans[s][1]>=intervals[i][0]){
+//                 ans[s][1]=max(ans[s][1],intervals[i][1]);
+//             }
+//             else{
+//                 ans.push_back({intervals[i][0],intervals[i][1]});
+//             }
+//         }
+
+//         return ans;
+
+//     }
+// };
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        if(intervals.empty()){
+            return {newInterval};
+        }
+
+        int i = 0;
+
+        while(i < intervals.size()){
+
+            // newInterval comes before current interval
+            if(newInterval[1] < intervals[i][0]){
+                intervals.insert(intervals.begin() + i, newInterval);
+                break;
+            }
+
+            // newInterval is completely inside current interval
+            else if(newInterval[0] >= intervals[i][0] &&
+                    newInterval[1] <= intervals[i][1]){
+                break;
+            }
+
+            // overlap: newInterval starts before current interval
+            else if(newInterval[0] < intervals[i][0] &&
+                    newInterval[1] >= intervals[i][0]){
+                intervals[i][0] = newInterval[0];
+                intervals[i][1] = max(intervals[i][1], newInterval[1]);
+                break;
+            }
+
+            // overlap: newInterval starts inside current interval
+            else if(newInterval[0] >= intervals[i][0] &&
+                    newInterval[0] <= intervals[i][1]){
+                intervals[i][1] = max(intervals[i][1], newInterval[1]);
+                break;
+            }
+
+            i++;
+        }
+
+        // newInterval comes after all intervals
+        if(i == intervals.size()){
+            intervals.push_back(newInterval);
+        }
+
+        ans.push_back(intervals[0]);
+
+        for(int i = 1; i < intervals.size(); i++){
+            int s = ans.size() - 1;
+
+            if(ans[s][1] >= intervals[i][0]){
+                ans[s][1] = max(ans[s][1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+
+        return ans;
+    }
+};
