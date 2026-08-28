4a5e4df37d992328d712a3e6212dfcf5d5a5630d
Sayeedullah <sayeedullahalom@gmail.com>
2026-08-28 19:25:01 +0530

    Time: 528 ms (94.54%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select round(count(*)/(select count(distinct player_id) from Activity),2) as fraction
+from Activity
+where (player_id,event_date) in (
+    select player_id,date_add(min(event_date),interval 1 day)
+    from Activity
+    group by player_id
+);
